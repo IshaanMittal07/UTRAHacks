585e27eef5342f81388194c3cd5e70d87599bd0d
Elil Thirumugam <126423230+Cool-1-2-3-4-5@users.noreply.github.com>
2026-01-31 22:13:28 -0500

    Arm Commit

---
+++ Arm.ino
@@ -0,0 +1,16 @@
+#include <Servo.h>
+Servo myServo;  // Create a servo object
+void setup() {
+  myServo.attach(11);  // Connect the signal wire to Digital Pin 9
+}
+
+void loop() {
+  myServo.write(0);   // Move to 0 degrees
+  delay(1000);        // Wait 1 second
+
+  myServo.write(90);  // Move to 90 degrees
+  delay(1000);
+
+  myServo.write(180); // Move to 180 degrees
+  delay(1000);
+}
